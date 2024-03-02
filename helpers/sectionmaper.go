fdcf0997978f8b887d5231d873eddb98e67c7a25
PailosNicolas <Pailosnicolas@gmail.com>
2024-03-02 19:58:02 +0000

    Added sectionmaper to helpers

---
+++ helpers/sectionmaper.go
@@ -0,0 +1,26 @@
+package helpers
+
+import "encoding/binary"
+
+func CreateSectionsMap(savedata []byte) map[int]Section {
+	sections := make(map[int]Section)
+	for i := 0; i < 14; i++ {
+		ix := i * 4096
+		section := savedata[ix : ix+4096]
+		footer := section[4084:]
+		id := int(binary.LittleEndian.Uint16(footer[0:2]))
+		index := binary.LittleEndian.Uint32(footer[4:8])
+		sections[id] = Section{
+			ID:       id,
+			Index:    index,
+			Contents: section[0:3968],
+		}
+	}
+	return sections
+}
+
+type Section struct {
+	ID       int
+	Index    uint32
+	Contents []byte
+}
