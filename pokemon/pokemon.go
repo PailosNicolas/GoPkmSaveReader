8a07a2565a5e5230dab3584db345b5ee2e616267
PailosNicolas <Pailosnicolas@gmail.com>
2024-03-03 22:37:25 +0000

    Added more data to pokemon struct

--- pokemon/pokemon.go
+++ pokemon/pokemon.go
@@ -1,20 +1,69 @@
 package pokemon
 
-import "github.com/PailosNicolas/GoPkmSaveReader/helpers"
+import (
+	"encoding/binary"
+
+	"github.com/PailosNicolas/GoPkmSaveReader/helpers"
+)
 
 type Pokemon struct {
-	OTName   string
-	Nickname string
+	PersonalityValue int
+	OTPublicId       int
+	OTSecretId       int
+	OTName           string
+	Nickname         string
+	Species          string
+	SpeciesIndex     int
 }
 
 func ParsePokemon(pkmData []byte) Pokemon {
+	orders := [24]string{"GAEM", "GAME", "GEAM", "GEMA", "GMAE", "GMEA", "AGEM", "AGME", "AEGM", "AEMG", "AMGE", "AMEG", "EGAM", "EGMA", "EAGM", "EAMG", "EMGA", "EMAG", "MGAE", "MGEA", "MAGE", "MAEG", "MEGA", "MEAG"}
 	pkm := Pokemon{}
+	var growth []byte
+	// var attacks []byte
+	// var evsAndCondition []byte
+	// var misc []byte
+
+	//personality value
+	personalityValue := pkmData[0:4]
+	pkm.PersonalityValue = int(binary.LittleEndian.Uint32(personalityValue))
+
+	//trianer full id
+	trainerId := pkmData[4:8]
+	pkm.OTPublicId = int(binary.LittleEndian.Uint16(pkmData[6:8]))
+	pkm.OTSecretId = int(binary.LittleEndian.Uint16(pkmData[4:6]))
+
+	//order
+	order := orders[pkm.PersonalityValue%24]
 
 	//getting ot
 	pkm.OTName = helpers.ReadString(pkmData[20:27])
 
 	//getting nickname
 	pkm.Nickname = helpers.ReadString(pkmData[8:18])
+
+	//subdata
+	key := helpers.XorBytes(personalityValue, trainerId)
+	subdata := pkmData[32:80]
+
+	for i := range 4 {
+		start := i * 12
+		if order[i] == 'G' {
+			growth = helpers.DecryptData(subdata[start:start+12], key)
+		}
+		if order[i] == 'A' {
+			_ = helpers.DecryptData(subdata[start:start+12], key)
+		}
+		if order[i] == 'E' {
+			_ = helpers.DecryptData(subdata[start:start+12], key)
+		}
+		if order[i] == 'M' {
+			_ = helpers.DecryptData(subdata[start:start+12], key)
+		}
+	}
+
+	pkm.SpeciesIndex = int(binary.LittleEndian.Uint16(growth[0:2]))
+	pkm.Species = helpers.Species[pkm.SpeciesIndex]
 
 	return pkm
 }
