615cc87ca080eb988a1ad228f790c0687dfa3b01
PailosNicolas <Pailosnicolas@gmail.com>
2024-03-09 00:53:02 +0000

    Added bytes operations

---
+++ helpers/bytesoperations.go
@@ -0,0 +1,33 @@
+package helpers
+
+func XorBytes(a, b []byte) []byte {
+	if len(a) != len(b) {
+		panic("byte slices must have equal length")
+	}
+	result := make([]byte, len(a))
+	for i := range a {
+		result[i] = a[i] ^ b[i]
+	}
+	return result
+}
+
+func Uint16ToBits(value uint16) []int {
+	bits := make([]int, 16)
+
+	for i := 0; i < 16; i++ {
+		bit := (value >> i) & 1
+		bits[i] = int(bit)
+	}
+
+	return bits
+}
+
+func BitsToInt(bits []int) int {
+	result := 0
+
+	for i := len(bits) - 1; i >= 0; i-- {
+		result = (result << 1) + bits[i]
+	}
+
+	return result
+}
