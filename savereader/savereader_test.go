150331bce5bd8a24ae4b47ffa682625bd54b98e0
PailosNicolas <Pailosnicolas@gmail.com>
2024-05-02 02:30:27 +0000

    Updated tests

--- savereader/savereader_test.go
+++ savereader/savereader_test.go
@@ -15,12 +15,12 @@ func TestReadDataFromSave(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 
-	if save.Trainer.Name != "Fry" {
-		t.Error("name should be Fry but is:", save.Trainer.Name)
+	if save.Trainer.Name() != "Fry" {
+		t.Error("name should be Fry but is:", save.Trainer.Name())
 	}
 
-	if save.Trainer.Gender != "Boy" {
-		t.Error("gender should be Boy but is:", save.Trainer.Gender)
+	if save.Trainer.Gender() != "Boy" {
+		t.Error("gender should be Boy but is:", save.Trainer.Gender())
 	}
 
 	// Error Short file
