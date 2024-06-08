93990727d4420c724f8eb45cdf3517aee8c9dd43
PailosNicolas <Pailosnicolas@gmail.com>
2024-06-08 22:18:37 +0000

    Added list of natures

---
+++ pokemon/natures.go
@@ -0,0 +1,229 @@
+package pokemon
+
+var natures = []Nature{
+	{
+		ID: 0,
+
+		Name: "Hardy",
+
+		Increases: "Attack",
+
+		Decreases: "Attack",
+	},
+	{
+		ID: 1,
+
+		Name: "Lonely",
+
+		Increases: "Attack",
+
+		Decreases: "Defense",
+	},
+	{
+		ID: 2,
+
+		Name: "Brave",
+
+		Increases: "Attack",
+
+		Decreases: "Speed",
+	},
+	{
+		ID: 3,
+
+		Name: "Adamant",
+
+		Increases: "Attack",
+
+		Decreases: "Sp. Attack",
+	},
+	{
+		ID: 4,
+
+		Name: "Naughty",
+
+		Increases: "Attack",
+
+		Decreases: "Sp. Defense",
+	},
+	{
+		ID: 5,
+
+		Name: "Bold",
+
+		Increases: "Defense",
+
+		Decreases: "Attack",
+	},
+	{
+		ID: 6,
+
+		Name: "Docile",
+
+		Increases: "Defense",
+
+		Decreases: "Defense",
+	},
+	{
+		ID: 7,
+
+		Name: "Relaxed",
+
+		Increases: "Defense",
+
+		Decreases: "Speed",
+	},
+	{
+		ID: 8,
+
+		Name: "Impish",
+
+		Increases: "Defense",
+
+		Decreases: "Sp. Attack",
+	},
+	{
+		ID: 9,
+
+		Name: "Lax",
+
+		Increases: "Defense",
+
+		Decreases: "Sp. Defense",
+	},
+	{
+		ID: 10,
+
+		Name: "Timid",
+
+		Increases: "Speed",
+
+		Decreases: "Attack",
+	},
+	{
+		ID: 11,
+
+		Name: "Hasty",
+
+		Increases: "Speed",
+
+		Decreases: "Defense",
+	},
+	{
+		ID: 12,
+
+		Name: "Serious",
+
+		Increases: "Speed",
+
+		Decreases: "Speed",
+	},
+	{
+		ID: 13,
+
+		Name: "Jolly",
+
+		Increases: "Speed",
+
+		Decreases: "Sp. Attack",
+	},
+	{
+		ID: 14,
+
+		Name: "Naive",
+
+		Increases: "Speed",
+
+		Decreases: "Sp. Defense",
+	},
+	{
+		ID: 15,
+
+		Name: "Modest",
+
+		Increases: "Sp. Attack",
+
+		Decreases: "Attack",
+	},
+	{
+		ID: 16,
+
+		Name: "Mild",
+
+		Increases: "Sp. Attack",
+
+		Decreases: "Defense",
+	},
+	{
+		ID: 17,
+
+		Name: "Quiet",
+
+		Increases: "Sp. Attack",
+
+		Decreases: "Speed",
+	},
+	{
+		ID: 18,
+
+		Name: "Bashful",
+
+		Increases: "Sp. Attack",
+
+		Decreases: "Sp. Attack",
+	},
+	{
+		ID: 19,
+
+		Name: "Rash",
+
+		Increases: "Sp. Attack",
+
+		Decreases: "Sp. Defense",
+	},
+	{
+		ID: 20,
+
+		Name: "Calm",
+
+		Increases: "Sp. Defense",
+
+		Decreases: "Attack",
+	},
+	{
+		ID: 21,
+
+		Name: "Gentle",
+
+		Increases: "Sp. Defense",
+
+		Decreases: "Defense",
+	},
+	{
+		ID: 22,
+
+		Name: "Sassy",
+
+		Increases: "Sp. Defense",
+
+		Decreases: "Speed",
+	},
+	{
+		ID: 23,
+
+		Name: "Careful",
+
+		Increases: "Sp. Defense",
+
+		Decreases: "Sp. Attack",
+	},
+	{
+		ID: 24,
+
+		Name: "Quirky",
+
+		Increases: "Sp. Defense",
+
+		Decreases: "Sp. Defense",
+	},
+}
