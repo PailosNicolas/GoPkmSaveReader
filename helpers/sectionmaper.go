496173737c05713885b73637d200e96c12cbda82
PailosNicolas <Pailosnicolas@gmail.com>
2024-08-25 01:59:24 +0000

    Added CheckSum to Section

--- helpers/sectionmaper.go
+++ helpers/sectionmaper.go
@@ -10,10 +10,12 @@ func CreateSectionsMap(savedata []byte) map[int]Section {
 		footer := section[4084:]
 		id := int(binary.LittleEndian.Uint16(footer[0:2]))
 		index := binary.LittleEndian.Uint32(footer[4:8])
+		checkSum := binary.LittleEndian.Uint16(footer[2:4])
 		sections[id] = Section{
 			ID:       id,
 			Index:    index,
 			Contents: section[0:3968],
+			CheckSum: checkSum,
 		}
 	}
 	return sections
@@ -23,4 +25,5 @@ type Section struct {
 	ID       int
 	Index    uint32
 	Contents []byte
+	CheckSum uint16
 }
