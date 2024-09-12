5b506b8139886d6cbe18d78a6e535350317a1875
PailosNicolas <Pailosnicolas@gmail.com>
2024-09-12 12:09:30 +0000

    Added replacepokemoninteam test

--- savereader/savereader_test.go
+++ savereader/savereader_test.go
@@ -23,6 +23,21 @@ func TestReadDataFromSave(t *testing.T) {
 		t.Error("gender should be Boy but is:", save.Trainer.Gender())
 	}
 
+	// Replace pokemon in team
+	save, err = save.ReplacePokemonInTeam(save.Trainer.Team()[0], 5)
+
+	if err != nil {
+		t.Error("Error replacing pokemon in team")
+	}
+
+	firstTeam := save.Trainer.Team()[0]
+
+	fifthTeam := save.Trainer.Team()[4]
+
+	if firstTeam.PersonalityValue() != fifthTeam.PersonalityValue() {
+		t.Error("Error replacing pokemon in team")
+	}
+
 	// Error Short file
 	pathShort := "../testfiles/Lola.pkm"
 
