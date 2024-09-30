d1c160929b94b2e95b1506b0b933a82ad6538e1e
PailosNicolas <Pailosnicolas@gmail.com>
2024-09-30 19:57:55 +0000

    Added more evolution validation entries

--- helpers/evolution_validations.go
+++ helpers/evolution_validations.go
@@ -7,4 +7,60 @@ var EvolutionValidation = map[string]map[string]string{
 		"method": MethodLvlUp,
 		"level":  "16",
 	},
+	"Ivysaur": {
+		"method": MethodLvlUp,
+		"level":  "32",
+	},
+	"Charmander": {
+		"method": MethodLvlUp,
+		"level":  "16",
+	},
+	"Charmeleon": {
+		"method": MethodLvlUp,
+		"level":  "36",
+	},
+	"Squirtle": {
+		"method": MethodLvlUp,
+		"level":  "16",
+	},
+	"Wartortle": {
+		"method": MethodLvlUp,
+		"level":  "36",
+	},
+	"Caterpie": {
+		"method": MethodLvlUp,
+		"level":  "7",
+	},
+	"Metapod": {
+		"method": MethodLvlUp,
+		"level":  "10",
+	},
+	"Weedle": {
+		"method": MethodLvlUp,
+		"level":  "7",
+	},
+	"Kakuna": {
+		"method": MethodLvlUp,
+		"level":  "10",
+	},
+	"Pidgey": {
+		"method": MethodLvlUp,
+		"level":  "18",
+	},
+	"Pidgeotto": {
+		"method": MethodLvlUp,
+		"level":  "36",
+	},
+	"Rattata": {
+		"method": MethodLvlUp,
+		"level":  "20",
+	},
+	"Spearow": {
+		"method": MethodLvlUp,
+		"level":  "20",
+	},
+	"Ekans": {
+		"method": MethodLvlUp,
+		"level":  "22",
+	},
 }
