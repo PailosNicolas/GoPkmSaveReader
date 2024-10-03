d566f85fb37591ccb5415bd4dfd5ca53b0ac5bbf
PailosNicolas <Pailosnicolas@gmail.com>
2024-10-03 22:25:19 +0000

    Added target id to evolutionvalidations

--- helpers/evolution_validations.go
+++ helpers/evolution_validations.go
@@ -10,118 +10,164 @@ var EvolutionValidation = map[string]map[string]map[string]string{
 		"Ivysaur": {
 			"method": MethodLvlUp,
 			"level":  "16",
+			"id":     "2",
 		},
 	},
 	"Ivysaur": {
 		"Venusaur": {
 			"method": MethodLvlUp,
 			"level":  "32",
+			"id":     "3",
 		},
 	},
 	"Charmander": {
 		"Charmeleon": {
 			"method": MethodLvlUp,
 			"level":  "16",
+			"id":     "5",
 		},
 	},
 	"Charmeleon": {
 		"Charizard": {
 			"method": MethodLvlUp,
 			"level":  "36",
+			"id":     "6",
 		},
 	},
 	"Squirtle": {
 		"Wartortle": {
 			"method": MethodLvlUp,
 			"level":  "16",
+			"id":     "8",
 		},
 	},
 	"Wartortle": {
 		"Blastoise": {
 			"method": MethodLvlUp,
 			"level":  "36",
+			"id":     "9",
 		},
 	},
 	"Caterpie": {
 		"Metapod": {
 			"method": MethodLvlUp,
 			"level":  "7",
+			"id":     "11",
 		},
 	},
 	"Metapod": {
 		"Butterfree": {
 			"method": MethodLvlUp,
 			"level":  "10",
+			"id":     "12",
 		},
 	},
 	"Weedle": {
 		"Kakuna": {
 			"method": MethodLvlUp,
 			"level":  "7",
+			"id":     "14",
 		},
 	},
 	"Kakuna": {
 		"Beedrill": {
 			"method": MethodLvlUp,
 			"level":  "10",
+			"id":     "15",
 		},
 	},
 	"Pidgey": {
 		"Pidgeotto": {
 			"method": MethodLvlUp,
 			"level":  "18",
+			"id":     "17",
 		},
 	},
 	"Pidgeotto": {
 		"Pidgeot": {
 			"method": MethodLvlUp,
 			"level":  "36",
+			"id":     "18",
 		},
 	},
 	"Rattata": {
 		"Raticate": {
 			"method": MethodLvlUp,
 			"level":  "20",
+			"id":     "20",
 		},
 	},
 	"Spearow": {
 		"Fearow": {
 			"method": MethodLvlUp,
 			"level":  "20",
+			"id":     "22",
 		},
 	},
 	"Ekans": {
 		"Arbok": {
 			"method": MethodLvlUp,
 			"level":  "22",
+			"id":     "24",
 		},
 	},
 	"Pichu": {
 		"Pikachu": {
 			"method":     MethodFriendship,
 			"friendship": "220",
+			"id":         "25",
 		},
 	},
 	"Pikachu": {
 		"Raichu": {
 			"method": MethodStone,
+			"id":     "26",
 		},
 	},
 	"Sandshrew": {
 		"Sandslash": {
 			"method": MethodLvlUp,
 			"level":  "22",
+			"id":     "28",
 		},
 	},
 	"Geodude": { // Added early for test
 		"Graveler": {
 			"method": MethodLvlUp,
 			"level":  "25",
+			"id":     "75",
 		},
 	},
 	"Graveler": {
 		"Golem": {
 			"method": MethodTrade,
+			"id":     "76",
+		},
+	},
+	"Nidoran-f": {
+		"Nidorina": {
+			"method": MethodLvlUp,
+			"level":  "16",
+			"id":     "30",
+		},
+	},
+	"Nidorina": {
+		"Nidoqueen": {
+			"method": MethodStone,
+			"id":     "31",
+		},
+	},
+	"Nidoran-m": {
+		"Nidorino": {
+			"method": MethodLvlUp,
+			"level":  "16",
+			"id":     "33",
+		},
+	},
+	"Nidorino": {
+		"Nidoking": {
+			"method": MethodStone,
+			"id":     "34",
 		},
 	},
 }
